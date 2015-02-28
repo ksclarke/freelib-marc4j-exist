bdfc99cf76d6ab8538012a9783533f6d4308ac58
Kevin S. Clarke <ksclarke@gmail.com>
2015-02-28 00:30:58 -0500

    Moar windowizing

--- src/test/scripts/load.bat
+++ src/test/scripts/load.bat
@@ -2,5 +2,5 @@
 
 call env.bat
 
-type "%MAVEN_BASEDIR%"\target\xq\load.xq | "%JAVA_HOME%"\bin\java -Dexist.home="%EXIST_HOME%"^^
+type "%MAVEN_BASEDIR%"\target\xq\load.xq | "%JAVA_HOME%"\bin\java -Dexist.home="%EXIST_HOME%" ^^
   -jar "%EXIST_HOME%"\start.jar client -l -u $%EXIST_USERNAME% %EXIST_PASSWORD_ARG% -x
--- src/test/scripts/test.bat
+++ src/test/scripts/test.bat
@@ -2,10 +2,10 @@
 
 call env.bat
 
-type "%MAVEN_BASEDIR%"\target\xq\test-write-to-file.xq | "%JAVA_HOME%"\bin\java -Dexist.home="%EXIST_HOME%"^^
-  -Dlog4j.configuration=file:"%MAVEN_BASEDIR%"\src\test\resources\log4j.xml^^
+type "%MAVEN_BASEDIR%"\target\xq\test-write-to-file.xq | "%JAVA_HOME%"\bin\java -Dexist.home="%EXIST_HOME%" ^^
+  -Dlog4j.configuration=file:"%MAVEN_BASEDIR%"\src\test\resources\log4j.xml ^^
   -jar "%EXIST_HOME%"\start.jar client -l -u %EXIST_USERNAME% %EXIST_PASSWORD_ARG% -x
   
-type "%MAVEN_BASEDIR"\target\xq\test-read-from-file.xq | "%JAVA_HOME%"\bin\java -Dexist.home="%EXIST_HOME%"^^
-  -Dlog4j.configuration=file:"%MAVEN_BASEDIR%"\src\test\resources\log4j.xml^^
+type "%MAVEN_BASEDIR"\target\xq\test-read-from-file.xq | "%JAVA_HOME%"\bin\java -Dexist.home="%EXIST_HOME%" ^^
+  -Dlog4j.configuration=file:"%MAVEN_BASEDIR%"\src\test\resources\log4j.xml ^^
   -jar "%EXIST_HOME%"\start.jar client -l -u %EXIST_USERNAME% %EXIST_PASSWORD_ARG% -x
